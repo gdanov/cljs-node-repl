eb7564e1126b22adfa453968d3dff765f802ec03
gdanov <georgi.danov@jumio.com>
2015-03-25 08:32:55 +0100

    #

--- src/repl/two.cljs
+++ src/repl/two.cljs
@@ -1,11 +1,8 @@
 (ns repl.two
-	(:require [clojure.string :refer [split-lines]])
-	)
+	(:require [clojure.string :refer [split-lines]]))
 
 (defn two []
 	(println "two++!!")
 	(println (split-lines "one\ntwo")))
 
-
 (two)
-;(println (namespace ::x))
